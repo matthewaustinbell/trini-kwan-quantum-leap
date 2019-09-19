01fce3ce4fb3f131103b7fbe46015ddc3a6f325e
Matt Gill <mtgill90@outlook.com>
2019-09-18 19:01:03 -0500

    wip

---
+++ TriniKwanQuantumLeap/Data/EventRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace TriniKwanQuantumLeap.Data
+{
+    class EventRepository
+    {
+        static List<Event> _events = new List<Event>();
+    }
+
+    class Event
+    {
+        public Guid Id { get; set; }
+        public bool IsPutRight { get; set; }
+
+        readonly string _location;
+        readonly string _date;
+        readonly string _host;
+
+    }
+}
