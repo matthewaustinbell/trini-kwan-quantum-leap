3693d76c83226b5a3b1dee4fdbaf890576a297a8
Matt Gill <mtgill90@outlook.com>
2019-09-18 19:52:27 -0500

    event repository and event class started, getAll and update methods work

--- TriniKwanQuantumLeap/Program.cs
+++ TriniKwanQuantumLeap/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using TriniKwanQuantumLeap.Data;
 
 namespace TriniKwanQuantumLeap
 {
@@ -10,6 +11,47 @@ namespace TriniKwanQuantumLeap
         static void Main(string[] args)
         {
             var eventRepository = new EventRepository();
+
+            var event1 = new Event
+            {
+                Location = "Nashville",
+                Date = "09/19/2019",
+                Host = "Your Mom",
+                IsPutRight = false,
+            };
+            eventRepository.AddEvent(event1);
+
+            var event2 = new Event
+            {
+                Location = "Memphis",
+                Date = "09/19/1990",
+                Host = "Larry King",
+                IsPutRight = false,
+            };
+            eventRepository.AddEvent(event2);
+
+            var event3 = new Event
+            {
+                Location = "Knoxville",
+                Date = "09/19/1800",
+                Host = "Oprah Winfrey",
+                IsPutRight = false,
+            };
+            eventRepository.AddEvent(event3);
+
+            var allEvents = eventRepository.GetAllEvents();
+
+            eventRepository.UpdateEvent(event1.Id);
+
+            foreach (var singleEvent in allEvents)
+            {
+                Console.WriteLine($"Location: {singleEvent.Location}");
+                Console.WriteLine($"Date: {singleEvent.Date}");
+                Console.WriteLine($"Host: {singleEvent.Host}");
+                Console.WriteLine($"Made Right? {singleEvent.IsPutRight}");
+                Console.WriteLine();
+            }
+
         }
     }
 }
