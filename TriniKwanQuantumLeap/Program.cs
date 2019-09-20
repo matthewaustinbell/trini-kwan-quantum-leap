59213365fb10f5c74babbae44523aee464cbb9b8
Mark Okafor <okaformark@gmail.com>
2019-09-20 10:22:33 -0500

    userInterface

--- TriniKwanQuantumLeap/Program.cs
+++ TriniKwanQuantumLeap/Program.cs
@@ -6,8 +6,26 @@ namespace TriniKwanQuantumLeap
     {
         static void Main(string[] args)
         {
-            var budget = new Budget();
-            budget.checkBalance();
+            Console.WriteLine("HELLO THERE! WOULD YOU LIKE TO TAKE A LEAP? REPLY WITH Y OR N");
+            string answer = Console.ReadLine().ToUpper();
+            while (true)
+            {
+                if (answer == "N")
+                {
+                    Console.WriteLine("FINE! BE BORING!");
+                    break;
+                }
+                
+                if (answer == "Y")
+                {
+                    var budget = new Budget();
+                    budget.checkBalance();
+                    break;
+                }
+                Console.WriteLine("PLEASE REPLY WITH A Y OR N");
+                answer = Console.ReadLine().ToUpper();
+            }
+
         }
     }
 }
